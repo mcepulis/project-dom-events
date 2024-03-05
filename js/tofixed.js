67b018583c96419528aac7073baeb83da3556e90
mcepulis <109752570+mcepulis@users.noreply.github.com>
2024-03-05 15:02:04 +0200

    tofixed completion

--- js/tofixed.js
+++ js/tofixed.js
@@ -2,30 +2,38 @@
 function toFixed(number, fractionLength) {
     const fraction = 10 ** fractionLength;
     const calc = Math.round(number * fraction) / fraction;
-    const integer = Math.floor(calc);
+    const integer = Math.trunc(calc);
     const decimal = Math.round((calc - integer) * fraction);
     let resultString = `${integer}`;
     if (fractionLength > 0) {
-        resultString += `.${String(decimal).padStart(fractionLength, '0')}`;
+        resultString += `.${String(Math.abs(decimal)).padStart(fractionLength, '0')}`;
     }
     return resultString;
 }
 
-const result = toFixed(2.548, 2);
-console.log(result); 
+console.log(toFixed(3, 0), '-->', '3');
+console.log(toFixed(-3, 0), '-->', '-3');
+console.log(toFixed(3.14, 0), '-->', '3');
+console.log(toFixed(-3.14, 0), '-->', '-3');
+console.log(toFixed(3.11, 1), '-->', '3.1');
+console.log(toFixed(-3.12, 1), '-->', '-3.1');
+console.log(toFixed(3.14, 5), '-->', '3.14000');
+console.log(toFixed(-3.14, 5), '-->', '-3.14000');
+console.log(toFixed(3.1412, 3), '-->', '3.141');
+console.log(toFixed(-3.1412, 3), '-->', '-3.141');
+console.log(toFixed(3.14, 2), '-->', '3.14');
+console.log(toFixed(-3.14, 2), '-->', '-3.14');
+console.log(toFixed(3.14159, 5), '-->', '3.14159');
+console.log(toFixed(-3.14159, 5), '-->', '-3.14159');
+console.log(toFixed(3.14, 4), '-->', '3.1400');
+console.log(toFixed(-3.14, 4), '-->', '-3.1400');
+console.log(toFixed(3.1412, 0), '-->', '3');
+console.log(toFixed(-3.1412, 0), '-->', '-3');
+console.log(toFixed(3.1, 2), '-->', '3.10');
+console.log(toFixed(-3.1, 2), '-->', '-3.10');
+console.log(toFixed(3, 3), '-->', '3.000');
+console.log(toFixed(-3, 3), '-->', '-3.000');
 
+console.log(toFixed(3.14159, 3), '-->', '3.142');
+console.log(toFixed(-3.14159, 3), '-->', '-3.142');
 
-// const number = 10;
-// const fractionLength = 3;
-// const fraction = 10 ** fractionLength;
-// const calc = Math.round(number * fraction) / fraction;
-// const integer = Math.floor(calc);
-// const decimal = Math.round((calc - integer) * fraction);
-// console.log(fraction);
-// console.log(calc);
-// console.log(integer);
-// console.log(decimal);
-// if (fractionLength > 0) {
-//             resultString = `.${String(decimal).padStart(fractionLength, '1')}`;
-//         }
-// console.log(resultString);
